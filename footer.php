b1f38ad93a5c2e0e3a82fc4a2a17bc2536507a14
Chandrakant <cks3383@gmail.com>
2018-02-22 19:22:24 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,32 @@
+<div class="bg-gray1 pt-4 mt-lg-3 pb-4">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-lg-4">
+				<p>
+				Home<br>
+				News<br>
+				About Us<br>
+				Contact Us<br>
+				Documentation<br>
+				</p>
+			</div>
+			<div class="col-sm-12 col-lg-8">
+				<p>
+					(C) 2016 - 2018, aryalinux.org. All Rights Reserved.
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="bg-gray2">
+	<div class="container">
+		
+	</div>
+</div>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ hero.php
@@ -0,0 +1,24 @@
+<div class="container-fluid bg-gray">
+  <div class="container">
+    <!-- Hero image -->
+
+    <div class="jumbotron">
+      <div class="row">
+        <div class="col-lg-8 col-sm-12">
+          <h2>AryaLinux 1.1 Released</h2>
+          <p>
+            AryaLinux 1.1 was released today with package updates and a built-from-scratch Gnome Desktop Environment. The Gnome 3 desktop environemnt is the prime focus of this release and we have taken great care to make sure that things simply run out of the box. You can read the full Release notes <a href="#">here</a>.
+          </p>
+        </div>
+        <div class="col-lg-4 col-sm-12">
+          <img src="images/desktop.png" class="img-fluid">
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col">
+          <a class="btn btn-primary btn-primary">Download Now</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
