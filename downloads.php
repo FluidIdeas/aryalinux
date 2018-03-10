198037ec4af4351302224bfaaf9c15021963f6ef
Chandrakant <cks3383@gmail.com>
2018-03-10 19:26:28 +0530

    Initial commit

--- downloads.php
+++
@@ -1,9 +0,0 @@
-<?php include_once('header.php');?>
-<?php include_once('menu.php');?>
-<div class="container mt-4 mb-4">
-	<h2>Downloads</h2>
-	<p>
-		
-	</p>
-</div>
-<?php include_once('footer.php');?>
