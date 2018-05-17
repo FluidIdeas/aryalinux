d39588afd8487a525959195d612d30f253904132
Chandrakant <aryalinux11@gmail.com>
2018-05-17 07:18:52 +0530

    Allow recommended dependencies from installing

--- base-system/apps/app.sh
+++ base-system/apps/app.sh
@@ -9,7 +9,7 @@ PACKAGE="$2"
 alps selfupdate
 alps updatescripts
 
-su - $USERNAME -c "PKG_BUILDER=$1 alps install--options n --packages $PACKAGE"
+su - $USERNAME -c "PKG_BUILDER=$1 alps install--options nr --packages $PACKAGE"
 if ! grep "$PACKAGE" /etc/alps/installed-list &> /dev/null
 then
 	echo "Application installation incomplete ($PACKAGE). Aborting..."
