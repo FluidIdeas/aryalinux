23925c17bfc236dbeb62a2090d38325e632840e4
Chandrakant <aryalinux11@gmail.com>
2018-05-24 00:05:15 +0530

    Fixed prepare-overlay.sh

--- base-system/prepare-overlay.sh
+++ base-system/prepare-overlay.sh
@@ -10,11 +10,7 @@ set +h
 DIR=$1
 
 if [ ! -d "$LFS/opt/$DIR" ]; then
-	mkdir -pv "$LFS/opt/$DIR"
-fi
-
-if [ "x$1" == "xgnome" ] || [ "x$2" == "xkde5" ] || [ "x$2" == "xxfce" ] || [ "x$2" == "xmate" ]; then
-	pushd "$LFS/opt/"
+	sudo mkdir -pv "$LFS/opt/$DIR"
 	sudo ln -svf "$DIR" "desktop-environment"
 	popd
 fi
