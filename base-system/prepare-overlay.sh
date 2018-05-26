2e6706598b70a0902511ff2b8fba9a7a5cf108d9
Chandrakant <aryalinux11@gmail.com>
2018-05-26 05:49:02 +0530

    fixed prepare-overlay.sh

--- base-system/prepare-overlay.sh
+++ base-system/prepare-overlay.sh
@@ -12,6 +12,8 @@ DIR=$1
 if [ ! -d "$LFS/opt/$DIR" ]; then
 	sudo mkdir -pv "$LFS/opt/$DIR"
 	pushd "$LFS/opt"
-	sudo ln -svf "$DIR" "desktop-environment"
+	if [ "x$DIR" != "xx-server" ]; then
+		sudo ln -svf "$DIR" "desktop-environment"
+	fi
 	popd
 fi
