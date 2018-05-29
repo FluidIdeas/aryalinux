ec1fca32eb99446f5d526055dc26d51f14fe7e91
Chandrakant <aryalinux11@gmail.com>
2018-05-29 22:15:56 +0530

    Fixed qt5-bin.sh

--- applications/qt5-bin.sh
+++ applications/qt5-bin.sh
@@ -6,16 +6,15 @@ set +h
 . /etc/alps/alps.conf
 . /var/lib/alps/functions
 
-NAME=qt5
-VERSION=5.8.0
-DESCRIPTION="Qt5 is a cross-platform application framework that is widely used for developing application software with a graphical user interface (GUI) (in 
-which cases Qt5 is classified as a widget toolkit), and also used for developing non-GUI programs such as command-line tools and consoles for servers. One of 
-the major users of Qt is KDE Frameworks 5 (KF5)."
+SOURCE_ONLY=n
+DESCRIPTION="br3ak Qt5 is a cross-platformbr3ak application framework that is widely used for developingbr3ak application software with a graphical user interface (GUI) (inbr3ak which cases Qt5 is classified as abr3ak widget toolkit), and also used for developing non-GUI programs suchbr3ak as command-line tools and consoles for servers. One of the majorbr3ak users of Qt is KDE Frameworks 5 (KF5).br3ak"
+SECTION="x"
+VERSION=5.10.1
+NAME="qt5"
 
-#REQ:python2
 #REQ:x7lib
 #REC:alsa-lib
-#REC:cacerts
+#REC:make-ca
 #REC:cups
 #REC:glib2
 #REC:gst10-plugins-base
@@ -29,9 +28,7 @@ the major users of Qt is KDE Frameworks 5 (KF5)."
 #REC:libxkbcommon
 #REC:mesa
 #REC:mtdev
-#REC:nss
-#REC:openssl10
-#REC:pcre
+#REC:pcre2
 #REC:sqlite
 #REC:wayland
 #REC:xcb-util-image
@@ -47,12 +44,28 @@ the major users of Qt is KDE Frameworks 5 (KF5)."
 #OPT:pulseaudio
 #OPT:unixodbc
 
-URL=https://sourceforge.net/projects/aryalinux-bin/files/releases/2017.06/bin/qt5.tar.xz
-TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
 
 cd $SOURCE_DIR
 
-wget -nc $URL
-sudo tar xf $TARBALL -C /
+URL=http://aryalinux.com/files/binaries/qt-5.10.1-x86_64.tar.xz
 
-register_installed "qt5" "$VERSION" "$INSTALLED_LIST"
+if [ ! -z $URL ]
+then
+wget -nc $URL
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+sudo tar xf $TARBALL -C /
+sudo mkdir -pv /opt/qt-5.10.1
+sudo ln -sfnv qt-5.10.1 /opt/qt5
+
+sudo tee /etc/ld.so.conf << EOF
+# Begin Qt addition
+/opt/qt5/lib
+# End Qt addition
+EOF
+sudo ldconfig
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
