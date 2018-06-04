a3abe69c682f325decd758f1f6b86021a5aacb84
Chandrakant <chandrakantsingh@protonmail.com>
2018-06-05 00:48:17 +0530

    Fixed thunar-archiver-plugin

--- applications/thunar-archive-plugin.sh
+++ applications/thunar-archive-plugin.sh
@@ -21,6 +21,7 @@ DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq)
 tar xf $TARBALL
 cd $DIRECTORY
 
+./autogen.sh --prefix=/usr &&
 GETTEXT_PACKAGE=thunar-archive-plugin ./configure --prefix=/usr &&
 make "-j$(nproc)" || make
 sudo make install
