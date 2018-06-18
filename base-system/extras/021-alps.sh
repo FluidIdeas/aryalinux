850f89327a1e4fba2138467020bbf8cab7b40057
Chandrakant <aryalinux11@gmail.com>
2018-06-18 09:08:44 +0530

    Fixed alps to include sourcelib and config libraries

--- base-system/extras/021-alps.sh
+++ base-system/extras/021-alps.sh
@@ -30,6 +30,8 @@ mkdir -pv /var/lib/alps
 cp -v functions /var/lib/alps/
 cp -v selfupdate.sh /var/lib/alps/
 cp -v updatescripts.sh /var/lib/alps/
+cp -v config.py /var/lib/alps/
+cp -v sourcelib.py /var/lib/alps/
 tar xf alps-scripts*.tar.gz -C /var/cache/alps/scripts/
 chmod a+x /usr/bin/alps
 chmod a+x /usr/bin/makepkg.sh
