23680d31f8809bc764ed0e2aa390733561a8066a
Chandrakant <chandrakantsingh@protonmail.com>
2018-06-23 09:27:28 +0530

    Fixed qt5

--- applications/qt5.sh
+++ applications/qt5.sh
@@ -80,17 +80,6 @@ sudo bash -e ./rootscript.sh
 sudo rm rootscript.sh
 
 
--archdatadir    /usr/lib/qt5                \
-            -bindir         /usr/bin                    \
-            -plugindir      /usr/lib/qt5/plugins        \
-            -importdir      /usr/lib/qt5/imports        \
-            -headerdir      /usr/include/qt5            \
-            -datadir        /usr/share/qt5              \
-            -docdir         /usr/share/doc/qt5          \
-            -translationdir /usr/share/qt5/translations \
-            -examplesdir    /usr/share/doc/qt5/examples
-
-
 ./configure -prefix /opt/qt5                          \
             -sysconfdir /etc/xdg                        \
             -confirm-license                            \
