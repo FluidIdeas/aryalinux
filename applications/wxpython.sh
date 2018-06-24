625a975701b2ad4630d82b699b20fa0dacba46f9
Chandrakant <chandrakantsingh@protonmail.com>
2018-06-24 21:09:26 +0530

    Install wxPython for both python2 and python3

--- applications/wxpython.sh
+++ applications/wxpython.sh
@@ -11,6 +11,7 @@ VERSION="latest"
 
 #REQ:wxwidgets
 
-sudo pip install -U wxPython
+sudo pip2 install -U wxPython
+sudo pip3 install -U wxPython
 
 register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
