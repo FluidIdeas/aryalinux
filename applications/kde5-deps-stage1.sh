83fb78b0ae089870693096c786ced2bfb9f34897
Chandrakant <chandrakantsingh@protonmail.com>
2018-07-06 23:55:13 +0530

    Added stage1 deps for kde5

---
+++ applications/kde5-deps-stage1.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+set -e
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+SOURCE_ONLY=n
+NAME="kde5-deps-stage1"
+DESCRIPTION="KDE Frameworks 5 is a collection of libraries based on top of Qt5 and QML derived from the monolithic KDE 4 libraries."
+VERSION=5.47
+
+packages="kitemmodels
+kitemviews
+kdoctools
+karchive
+kcodecs
+kconfig
+kcoreaddons
+kdbusaddons
+kguiaddons
+kidletime
+kimageformats
+kplotting
+kwidgetsaddons
+oxygen-icons
+kwindowsystem
+solid
+sonnet
+threadweaver
+ki18n"
+
+BASE_URL=http://download.kde.org/stable/frameworks/$VERSION/
+
+export KF5_PREFIX=/opt/kf5
+export QT5DIR=/opt/qt5
+
+pathappend $KF5_PREFIX/bin             PATH
+pathappend $KF5_PREFIX/lib/pkgconfig   PKG_CONFIG_PATH
+pathappend /etc/xdg                    XDG_CONFIG_DIRS
+pathappend $KF5_PREFIX/etc/xdg         XDG_CONFIG_DIRS
+pathappend /usr/share                  XDG_DATA_DIRS
+pathappend $KF5_PREFIX/share           XDG_DATA_DIRS
+pathappend $KF5_PREFIX/lib/plugins     QT_PLUGIN_PATH
+pathappend $KF5_PREFIX/lib/qml         QML2_IMPORT_PATH
+pathappend $KF5_PREFIX/lib/python2.7/site-packages PYTHONPATH
+pathappend $QT5DIR/plugins             QT_PLUGIN_PATH
+pathappend $QT5DIR/qml                 QML2_IMPORT_PATH
+
+
+sudo install -v -dm755           $KF5_PREFIX/{etc,share} &&
+sudo ln -sfv /etc/dbus-1         $KF5_PREFIX/etc         &&
+sudo ln -sfv /usr/share/dbus-1   $KF5_PREFIX/share
+sudo install -v -dm755                $KF5_PREFIX/share/icons &&
+sudo ln -sfv /usr/share/icons/hicolor $KF5_PREFIX/share/icons
+
+export KF5_PREFIX=/opt/kf5
+export QT5DIR=/opt/qt5
+
+
+mkdir -pv $NAME
+pushd $NAME
+touch build-log
+
+for package in $packages; do
+	if ! grep $package build-log &> /dev/null; then
+		wget -nc $BASE_URL/$package-$VERSION.0.tar.xz
+		TARBALL="$package-$VERSION.0.tar.xz"
+		DIRECTORY="$package-$VERSION.0"
+		tar xf $TARBALL
+		pushd $DIRECTORY
+		mkdir build
+		cd    build
+		cmake -DCMAKE_INSTALL_PREFIX=$KF5_PREFIX \
+			-DCMAKE_PREFIX_PATH=$QT5DIR        \
+			-DCMAKE_BUILD_TYPE=Release         \
+			-DLIB_INSTALL_DIR=lib              \
+			-DBUILD_TESTING=OFF                \
+			-Wno-dev ..
+		make "-j`nproc`"
+		sudo make install
+		popd
+
+		echo "$package=>`date`" >> build-log
+
+		rm -rf $DIRECTORY
+	else
+		echo "$package installed. Skipping."
+	fi
+done
+
+popd
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
