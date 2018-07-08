320cc4d3c3d0dffb3da1fe120e2d4605fd470d43
Chandrakant <cks3383@gmail.com>
2018-07-08 19:07:35 +0530

    Fixed permissions issue with virtualbox

--- applications/virtualbox.sh
+++ applications/virtualbox.sh
@@ -10,6 +10,8 @@ set +h
 
 cd $SOURCE_DIR
 
+export KERNEL_VERSION=$(uname -r)
+
 if [ `uname -m` == "x86_64" ]
 then
 	URL=https://download.virtualbox.org/virtualbox/5.2.14/VirtualBox-5.2.14-123301-Linux_amd64.run
@@ -17,7 +19,7 @@ else
 	URL=https://download.virtualbox.org/virtualbox/5.2.14/VirtualBox-5.2.14-123301-Linux_x86.run
 fi
 
-KERNEL_URL=https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.17.3.tar.xz
+KERNEL_URL=https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-$KERNEL_VERSION.tar.xz
 KERNEL_TARBALL=$(echo $KERNEL_URL | rev | cut -d/ -f1 | rev)
 VBOX_INSTALLER=$(echo $URL | rev | cut -d/ -f1 | rev)
 
@@ -28,7 +30,7 @@ KERNEL_DIR=$(tar tf $KERNEL_TARBALL | cut -d/ -f1 | uniq)
 
 mkdir -pv /usr/src/
 sudo tar xf $KERNEL_TARBALL -C /usr/src
-sudo ln -svf /usr/src/$KERNEL_DIR /lib/modules/4.16.10/build
+sudo ln -svf /usr/src/$KERNEL_DIR /lib/modules/$KERNEL_VERSION/build
 
 pushd /usr/src/$KERNEL_DIR
 sudo make oldconfig
