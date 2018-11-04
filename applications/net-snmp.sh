e62e1ff81c188961041b936ed01c1fa79c7dd01a
Chandrakant <aryalinux11@gmail.com>
2018-11-04 07:55:03 +0530

    Bumped net-snmp

--- applications/net-snmp.sh
+++ applications/net-snmp.sh
@@ -7,21 +7,19 @@ set +h
 . /var/lib/alps/functions
 
 NAME="net-snmp"
-VERSION="5.7.3"
+VERSION="5.8"
 
 cd $SOURCE_DIR
 
-URL=downloads.sourceforge.net/net-snmp/net-snmp-5.7.3.tar.gz
+URL=https://sourceforge.net/projects/net-snmp/files/net-snmp/5.8/net-snmp-5.8.tar.gz
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 wget -nc $URL
-wget -nc https://sourceforge.net/projects/aryalinux-bin/files/artifacts/net-snmp-5.7.3-fixes.patch
 
 DIRECTORY=`tar tf $TARBALL | cut -d/ -f1 | uniq`
 
 tar xf $TARBALL
 cd $DIRECTORY
 
-patch -Np1 -i ../net-snmp-5.7.3-fixes.patch &&
 ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-static &&
 make "-j`nproc`"
 
