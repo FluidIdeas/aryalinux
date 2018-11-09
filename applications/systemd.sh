a953e131530c3a7b7342d3087f3c2da1d8ee28cd
Chandrakant Singh <chandrakant.singh@tavant.com>
2018-11-09 11:52:06 +0530

    Fixed systemd

--- applications/systemd.sh
+++ applications/systemd.sh
@@ -40,6 +40,7 @@ then
 wget -nc https://github.com/systemd/systemd/archive/v239/systemd-239.tar.gz || wget -nc http://mirrors-usa.go-parts.com/blfs/conglomeration/systemd/systemd-239.tar.gz || wget -nc http://mirrors-ru.go-parts.com/blfs/conglomeration/systemd/systemd-239.tar.gz || wget -nc ftp://ftp.lfs-matrix.net/pub/blfs/conglomeration/systemd/systemd-239.tar.gz || wget -nc http://ftp.lfs-matrix.net/pub/blfs/conglomeration/systemd/systemd-239.tar.gz || wget -nc ftp://ftp.osuosl.org/pub/blfs/conglomeration/systemd/systemd-239.tar.gz || wget -nc http://ftp.osuosl.org/pub/blfs/conglomeration/systemd/systemd-239.tar.gz
 wget -nc http://www.linuxfromscratch.org/patches/blfs/svn/systemd-239-glibc_statx_fix-1.patch || wget -nc http://www.linuxfromscratch.org/patches/downloads/systemd/systemd-239-glibc_statx_fix-1.patch
 wget -nc https://github.com/systemd/systemd/archive/v238/systemd-238.tar.gz
+wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/1.3/meson-rename-Ddebug-to-Ddebug-extra.patch
 
 TARBALL=`echo $URL | rev | cut -d/ -f1 | rev`
 if [ -z $(echo $TARBALL | grep ".zip$") ]; then
@@ -55,7 +56,7 @@ fi
 whoami > /tmp/currentuser
 
 patch -Np1 -i ../systemd-239-glibc_statx_fix-1.patch
-
+patch -Np1 -i ../meson-rename-Ddebug-to-Ddebug-extra.patch
 
 sed -i 's/GROUP="render", //' rules/50-udev-default.rules.in
 
