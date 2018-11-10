202f39b42472fc87d9c081c94f6814232ca57086
Chandrakant <aryalinux11@gmail.com>
2018-11-10 20:55:43 +0530

    Non interactive installation of packages for automation script

--- base-system/apps/essentials.sh
+++ base-system/apps/essentials.sh
@@ -7,19 +7,19 @@ USERNAME="$1"
 
 alps selfupdate
 alps updatescripts
-su - $USERNAME -c "PKG_BUILDER=$1 alps install profile python2 python3 nano openssl10 general_which wget cacerts ntfs-3g fuse lvm2 parted gptfdisk shadow"
+su - $USERNAME -c "PKG_BUILDER=$1 alps install -ni profile python2 python3 nano openssl10 general_which wget cacerts ntfs-3g fuse lvm2 parted gptfdisk shadow"
 if ! grep "shadow=" /etc/alps/installed-list &> /dev/null
 then
 	echo "Essentials incomplete (shadow). Aborting..."
 	exit 1
 fi
-PKG_BUILDER=$1 alps install sudo
+PKG_BUILDER=$1 alps install -ni sudo
 if ! grep "sudo=" /etc/alps/installed-list &> /dev/null
 then
         echo "Essentials incomplete (sudo). Aborting..."
         exit 1
 fi
-su - $USERNAME -c "PKG_BUILDER=$1 alps install usbutils pciutils openssh gobject-introspection libxml2 desktop-file-utils shared-mime-info ccache"
+su - $USERNAME -c "PKG_BUILDER=$1 alps install -ni usbutils pciutils openssh gobject-introspection libxml2 desktop-file-utils shared-mime-info ccache"
 if ! grep "ccache=" /etc/alps/installed-list &> /dev/null
 then
         echo "Essentials incomplete (ccache). Aborting..."
