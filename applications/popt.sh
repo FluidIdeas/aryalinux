8f89a7c0184abdf68cff64bc4e707735923d3216
Chandrakant <aryalinux11@gmail.com>
2019-01-10 22:10:01 +0530

    Fixed popt

--- applications/popt.sh
+++ applications/popt.sh
@@ -43,16 +43,6 @@ sudo /tmp/rootscript.sh
 sudo rm -rf /tmp/rootscript.sh
 
 
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-install -v -m755 -d /usr/share/doc/popt-1.16 &&
-install -v -m644 doxygen/html/* /usr/share/doc/popt-1.16
-ENDOFROOTSCRIPT
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
 
 register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
