7fb870d5caa0da1fe342b073dc3eef9a89ad0190
Chandrakant <aryalinux11@gmail.com>
2019-01-13 19:04:22 +0530

    Fixed ffmpeg

--- applications/ffmpeg.sh
+++ applications/ffmpeg.sh
@@ -85,17 +85,6 @@ sed -i 's/-lflite"/-lflite -lasound"/' configure &&
 make &&
 
 gcc tools/qt-faststart.c -o tools/qt-faststart
-pushd doc &&
-for DOCNAME in `basename -s .html *.html`
-do
-texi2pdf -b $DOCNAME.texi &&
-texi2dvi -b $DOCNAME.texi &&
-
-dvips -o $DOCNAME.ps \
-$DOCNAME.dvi
-done &&
-popd &&
-unset DOCNAME
 
 sudo rm -rf /tmp/rootscript.sh
 cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
@@ -109,33 +98,6 @@ chmod a+x /tmp/rootscript.sh
 sudo /tmp/rootscript.sh
 sudo rm -rf /tmp/rootscript.sh
 
-
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-install -v -m644 doc/*.pdf /usr/share/doc/ffmpeg-4.1 &&
-install -v -m644 doc/*.ps /usr/share/doc/ffmpeg-4.1
-ENDOFROOTSCRIPT
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-install -v -m755 -d /usr/share/doc/ffmpeg-4.1/api &&
-cp -vr doc/doxy/html/* /usr/share/doc/ffmpeg-4.1/api &&
-find /usr/share/doc/ffmpeg-4.1/api -type f -exec chmod -c 0644 \{} \; &&
-find /usr/share/doc/ffmpeg-4.1/api -type d -exec chmod -c 0755 \{} \;
-ENDOFROOTSCRIPT
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-make fate-rsync SAMPLES=fate-suite/
-<span class="command"><strong>rsync -vrltLW --delete --timeout=60 --contimeout=60 \
-rsync://fate-suite.ffmpeg.org/fate-suite/ fate-suite/</strong></span>
-make fate THREADS=<em class="replaceable"><code>N</code></em> SAMPLES=fate-suite/ | tee ../fate.log &&
-grep ^TEST ../fate.log | wc -l
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
 
