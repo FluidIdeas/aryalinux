6b296ded2ba4fe1d57925b04addc183f341aa109
Chandrakant <aryalinux11@gmail.com>
2019-01-13 12:09:08 +0530

    Disabled installation of mesa docs

--- applications/mesa.sh
+++ applications/mesa.sh
@@ -78,16 +78,6 @@ sudo /tmp/rootscript.sh
 sudo rm -rf /tmp/rootscript.sh
 
 
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-install -v -dm755 /usr/share/doc/mesa-18.3.1 &&
-cp -rfv docs/* /usr/share/doc/mesa-18.3.1
-ENDOFROOTSCRIPT
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
 
 register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
