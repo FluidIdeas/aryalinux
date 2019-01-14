f9d7365af230f7e64deda937d48c3543b6fc36f0
Chandrakant <aryalinux11@gmail.com>
2019-01-14 18:37:15 +0530

    Created gnome-desktop-environment

---
+++ applications/gnome-desktop-environment.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+#REQ:accountsservice
+#REQ:desktop-file-utils
+#REQ:gcr
+#REQ:gsettings-desktop-schemas
+#REQ:libsecret
+#REQ:rest
+#REQ:totem-pl-parser
+#REQ:vte
+#REQ:yelp-xsl
+#REQ:gconf
+#REQ:geocode-glib
+#REQ:gjs
+#REQ:gnome-desktop
+#REQ:gnome-menus
+#REQ:libnotify
+#REQ:gnome-online-accounts
+#REQ:gnome-video-effects
+#REQ:grilo
+#REQ:libchamplain
+#REQ:libgdata
+#REQ:libgee
+#REQ:libgtop
+#REQ:libgweather
+#REQ:libpeas
+#REQ:libwacom
+#REQ:libwnck
+#REQ:evolution-data-server
+#REQ:folks
+#REQ:gfbgraph
+#REQ:telepathy-glib
+#REQ:telepathy-logger
+#REQ:telepathy-mission-control
+#REQ:caribou
+#REQ:dconf
+#REQ:gnome-backgrounds
+#REQ:librsvg
+#REQ:gnome-themes-extra
+#REQ:gvfs
+#REQ:nautilus
+#REQ:zenity
+#REQ:gnome-bluetooth
+#REQ:gnome-keyring
+#REQ:clutter-gst
+#REQ:cups
+#REQ:cups-filters
+#REQ:gnome-settings-daemon
+#REQ:grilo
+#REQ:gnome-control-center
+#REQ:mutter
+#REQ:gnome-shell
+#REQ:gnome-shell-extensions
+#REQ:gnome-session
+#REQ:gdm
+#REQ:gnome-user-docs
+#REQ:baobab
+#REQ:brasero
+#REQ:cheese
+#REQ:eog
+#REQ:evince
+#REQ:file-roller
+#REQ:gedit
+#REQ:gnome-calculator
+#REQ:gnome-disk-utility
+#REQ:gnome-logs
+#REQ:gnome-maps
+#REQ:gnome-nettool
+#REQ:gnome-power-manager
+#REQ:gnome-system-monitor
+#REQ:gnome-terminal
+#REQ:gnome-weather
+#REQ:gucharmap
+#REQ:network-manager-applet
+#REQ:seahorse
+#REQ:notification-daemon
+#REQ:polkit-gnome
+#REQ:cups-filters
+#REQ:xdg-user-dirs
+#REQ:plank
+
+cd $SOURCE_DIR
+
+
+NAME=gnome-desktop-environment
+VERSION=3.30.2
+URL=""
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
