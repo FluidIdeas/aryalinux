ab0cff22135eb32e8aeacd16d5c67f4b8b7ada4e
Chandrakant <aryalinux11@gmail.com>
2019-01-15 08:12:25 +0530

    Fixed gst-plugins-bad

--- applications/gst10-plugins-bad.sh
+++ applications/gst10-plugins-bad.sh
@@ -47,6 +47,7 @@ set +h
 cd $SOURCE_DIR
 
 wget -nc https://gstreamer.freedesktop.org/src/gst-plugins-bad/gst-plugins-bad-1.14.4.tar.xz
+wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/1.4/gst-plugins-bad-1.14.4-fdkaac.patch
 
 NAME=gst10-plugins-bad
 VERSION=1.14.4
@@ -67,6 +68,7 @@ fi
 cd $DIRECTORY
 fi
 
+patch -Np1 -i ../gst-plugins-bad-1.14.4-fdkaac.patch
 ./configure --prefix=/usr \
 --disable-wayland \
 --disable-opencv \
