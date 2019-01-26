a4915aab8ba1c880785588eb6d61bd8337eb263e
Chandrakant <aryalinux11@gmail.com>
2019-01-26 23:04:01 +0530

    Added libtdb

---
+++ applications/libtdb.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+
+cd $SOURCE_DIR
+
+wget -nc https://www.samba.org/ftp/tdb/tdb-1.3.17.tar.gz
+
+NAME=libtdb
+VERSION=1.3.17
+URL=https://www.samba.org/ftp/tdb/tdb-1.3.17.tar.gz
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+sed -i "s@../../buildtools@buildtools@g" Makefile
+./configure --prefix=/usr &&
+make
+sudo make install
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
