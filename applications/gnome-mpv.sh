f6b3b6b4c4a85778f259cc290915840387653f0e
Chandrakant <aryalinux11@gmail.com>
2019-01-26 22:43:45 +0530

    Added mpv and gnome-mpv

---
+++ applications/gnome-mpv.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+#REQ:mpv
+#REQ:gnome-desktop-environment
+
+cd $SOURCE_DIR
+
+wget -nc https://github.com/gnome-mpv/gnome-mpv/releases/download/v0.16/gnome-mpv-0.16.tar.xz
+
+NAME=gnome-mpv
+VERSION=0.16
+URL=https://github.com/gnome-mpv/gnome-mpv/releases/download/v0.16/gnome-mpv-0.16.tar.xz
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+./configure --prefix=/usr &&
+make
+sudo make install
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
---
+++ applications/mpv.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+#REQ:xserver-meta
+#REQ:mesa
+#REQ:libasound
+#REQ:pulseaudio
+#REQ:libass
+#REQ:libjpeg
+#REQ:gnutls
+#REQ:x264
+#REQ:lame
+#REQ:fdk-aac
+
+cd $SOURCE_DIR
+
+wget -nc https://github.com/mpv-player/mpv/archive/v0.29.1.tar.gz
+
+NAME=mpv
+VERSION=0.29.1
+URL=https://github.com/mpv-player/mpv/archive/v0.29.1.tar.gz
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+./bootstrap.py &&
+./waf configure --enable-libmpv-shared --enable-vaapi --enable-vdpau --prefix=/usr &&
+./waf build &&
+sudo ./waf install --prefix=/usr
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
