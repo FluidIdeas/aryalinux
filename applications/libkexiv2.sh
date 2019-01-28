c22bfd4e1b46eeaca47c9933ec455f3e1124402e
Chandrakant <aryalinux11@gmail.com>
2019-01-28 23:30:44 +0530

    Fixed libkexiv2

--- applications/libkexiv2.sh
+++ applications/libkexiv2.sh
@@ -11,11 +11,11 @@ set +h
 
 cd $SOURCE_DIR
 
-wget -nc http://download.kde.org/stable/applications/18.12.1/src/libkexiv2-18.12.1.tar.xz
+wget -nc http://download.kde.org/stable/applications/18.12.0/src/libkexiv2-18.12.0.tar.xz
 
 NAME=libkexiv2
-VERSION=18.12.1
-URL=http://download.kde.org/stable/applications/18.12.1/src/libkexiv2-18.12.1.tar.xz
+VERSION=18.12.0
+URL=http://download.kde.org/stable/applications/18.12.0/src/libkexiv2-18.12.0.tar.xz
 
 if [ ! -z $URL ]
 then
