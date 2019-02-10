90b4be089819f905d8b6a723a52aba011f571c13
Chandrakant <aryalinux11@gmail.com>
2019-02-10 13:37:20 +0530

    Added jack2 and ssr

---
+++ applications/jack2.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+
+cd $SOURCE_DIR
+
+wget -nc https://github.com/jackaudio/jack2/releases/download/v1.9.12/jack2-1.9.12.tar.gz
+
+NAME=jack2
+VERSION=1.9.12
+URL=https://github.com/jackaudio/jack2/releases/download/v1.9.12/jack2-1.9.12.tar.gz
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+./waf configure --prefix=/usr &&
+./waf build
+sudo ./waf install
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
---
+++ applications/ssr.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+#REQ:qt5
+#REQ:jack2
+#REQ:ffmpeg
+#REQ:pulseaudio
+
+cd $SOURCE_DIR
+
+wget -nc https://github.com/MaartenBaert/ssr/archive/0.3.11.tar.gz
+
+NAME=ssr
+VERSION=0.3.11
+URL=https://github.com/MaartenBaert/ssr/archive/0.3.11.tar.gz
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+mkdir -pv build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DWITH_QT5=1 -DWITH_PULSEAUDIO=1 -DWITH_JACK=1 .. &&
+make
+sudo make install
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
