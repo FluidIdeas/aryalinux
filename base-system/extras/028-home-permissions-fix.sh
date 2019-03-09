0b0ddad414f7027d935090bdf73907ab872e698c
Chandrakant <aryalinux11@gmail.com>
2019-03-09 20:07:16 +0530

    Added the home-permissions-fix

---
+++ base-system/extras/028-home-permissions-fix.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /sources/build-properties
+
+SOURCE_DIR="/sources"
+LOGFILE="/sources/build-log"
+STEPNAME="028-home-permissions-fix"
+
+cd $SOURCE_DIR
+
+cat > /etc/profile.d/home-permissions-fix.sh <<EOF
+pushd /home
+for dir in *; do
+chown -R $dir:$dir dir
+done
+EOF
+
+echo "$STEPNAME" | tee -a $LOGFILE
+
+fi
