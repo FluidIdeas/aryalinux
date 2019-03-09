817a27cee007749979b72abb1246ffbd01f74e87
Chandrakant <aryalinux11@gmail.com>
2019-03-09 20:11:39 +0530

    fixed home-permissions-fix

--- base-system/extras/028-home-permissions-fix.sh
+++ base-system/extras/028-home-permissions-fix.sh
@@ -13,9 +13,10 @@ cd $SOURCE_DIR
 
 cat > /etc/profile.d/home-permissions-fix.sh <<EOF
 pushd /home
-for dir in *; do
-chown -R $dir:$dir dir
+for d in *; do
+chown -R $d:$d d
 done
+popd
 EOF
 
 echo "$STEPNAME" | tee -a $LOGFILE
