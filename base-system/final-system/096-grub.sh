afaecc352db3f10708bbd16db6012e23c213f80a
Chandrakant <aryalinux11@gmail.com>
2019-03-21 05:33:51 +0530

    Fixed the gurb script

--- base-system/final-system/096-grub.sh
+++ base-system/final-system/096-grub.sh
@@ -58,6 +58,7 @@ then
 fi
 
 patch -Np1 -i ../grub-2.02-gcc.patch
+patch -Np1 -i ../grub-2.02-relocation.patch
 sed -i "s@GNU GRUB  version %s@$OS_NAME $OS_VERSION $OS_CODENAME \- GNU GRUB@g" grub-core/normal/main.c
 
 if [ `uname -m` == "x86_64" ]
