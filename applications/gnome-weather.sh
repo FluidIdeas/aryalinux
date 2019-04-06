5e969343bbbe1ae20d1ecefb1bdc54ae207e10a1
Chandrakant <aryalinux11@gmail.com>
2019-04-06 11:25:24 +0530

    Fixed gnome-weather

--- applications/gnome-weather.sh
+++ applications/gnome-weather.sh
@@ -33,17 +33,12 @@ fi
 cd $DIRECTORY
 fi
 
-./configure --prefix=/usr &&
-make
+mkdir -pv build &&
+cd build
 
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-make install
-ENDOFROOTSCRIPT
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
+meson --prefix=/usr &&
+ninja
+sudo ninja install
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
 
