693f86ab9e9a1091ccada6f9a0ffa68da0d89863
Chandrakant <aryalinux11@gmail.com>
2019-04-09 02:28:30 +0530

    Fixed openvpn

--- applications/openvpn.sh
+++ applications/openvpn.sh
@@ -32,8 +32,8 @@ fi
 cd $DIRECTORY
 fi
 
-if grep "gnome-desktop-environment" /etc/alps/installed-list &> /dev/null; then WITH_GNOME="--with-gnome"; fi
-./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var &WITH_GNOME &&
+if grep "gnome-desktop-environment" /etc/alps/installed-list &> /dev/null; then export WITH_GNOME="--with-gnome"; fi
+./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var $WITH_GNOME &&
 make
 sudo make install
 
