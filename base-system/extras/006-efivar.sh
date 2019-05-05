398c95e30879c6c6f7b450629262d89eedfb5ecd
Chandrakant <aryalinux11@gmail.com>
2019-05-05 20:03:18 +0530

    Upgraded efivar

--- base-system/extras/006-efivar.sh
+++ base-system/extras/006-efivar.sh
@@ -13,7 +13,7 @@ fi
 SOURCE_DIR="/sources"
 LOGFILE="/sources/build-log"
 STEPNAME="006-efivar.sh"
-TARBALL="efivar-36.tar.bz2"
+TARBALL="efivar-37.tar.bz2"
 
 echo "$LOGLENGTH" > /sources/lines2track
 
