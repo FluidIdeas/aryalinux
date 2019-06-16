1d4254968887d7fc5e291e29ecc33e452274fb74
Chandrakant <aryalinux11@gmail.com>
2019-06-16 19:46:20 +0530

    Fixed ntfs-3g

--- applications/ntfs-3g.sh
+++ applications/ntfs-3g.sh
@@ -58,15 +58,6 @@ chmod a+x /tmp/rootscript.sh
 sudo /tmp/rootscript.sh
 sudo rm -rf /tmp/rootscript.sh
 
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-chmod -v 777 /mnt/usb
-ENDOFROOTSCRIPT
-
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
 
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
