8b881aa1ec2c7d31507e2341f56034558c136b58
Chandrakant <aryalinux11@gmail.com>
2019-06-19 22:48:08 +0530

    Added caribou

---
+++ applications/caribou.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+#REQ:clutter
+#REQ:gtk3
+#REQ:libgee
+#REQ:libxklavier
+#REQ:python-modules#pygobject2
+#REQ:python-modules#pygobject3
+#REQ:vala
+
+
+cd $SOURCE_DIR
+
+wget -nc http://ftp.gnome.org/pub/gnome/sources/caribou/0.4/caribou-0.4.21.tar.xz
+wget -nc ftp://ftp.gnome.org/pub/gnome/sources/caribou/0.4/caribou-0.4.21.tar.xz
+
+
+NAME=caribou
+VERSION=0.4.21
+URL=http://ftp.gnome.org/pub/gnome/sources/caribou/0.4/caribou-0.4.21.tar.xz
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+PYTHON=python3 ./configure --prefix=/usr --sysconfdir=/etc --disable-gtk2-module --disable-static && make
+
+sudo make install
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
+
