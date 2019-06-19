414d93c3906f14ed4baac9e01f352cb9e9af70d4
Chandrakant <aryalinux11@gmail.com>
2019-06-19 22:51:35 +0530

    Added alsa

---
+++ applications/alsa.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+#REQ:alsa-lib
+#REQ:alsa-plugins
+#REQ:alsa-utils
+#REQ:alsa-tools
+#REQ:alsa-firmware
+#REQ:alsa-oss
+
+
+cd $SOURCE_DIR
+
+
+
+NAME=alsa
+VERSION=1.1.9
+
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
+
