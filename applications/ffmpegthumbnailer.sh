46a56e7b847939ef1ba5922790b207df1d072246
Chandrakant <aryalinux11@gmail.com>
2019-06-23 08:10:08 +0530

    Added ffmpegthumbnailer

---
+++ applications/ffmpegthumbnailer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+
+#REQ:nautilus
+
+
+cd $SOURCE_DIR
+
+wget -nc https://github.com/dirkvdb/ffmpegthumbnailer/releases/download/2.2.0/ffmpegthumbnailer-2.2.0.tar.bz2
+
+
+NAME=ffmpegthumbnailer
+VERSION=2.2.0
+URL=https://github.com/dirkvdb/ffmpegthumbnailer/releases/download/2.2.0/ffmpegthumbnailer-2.2.0.tar.bz2
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+mkdir build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=/usr .. &&
+make
+
+sudo make install
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
+
