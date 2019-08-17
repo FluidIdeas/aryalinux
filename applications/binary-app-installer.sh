8e125d1407f458a9bc529414c71056f14752c881
Chandrakant <aryalinux11@gmail.com>
2019-08-17 17:49:35 +0530

    Added app-installer

---
+++ applications/binary-app-installer.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+. /etc/alps/directories.conf
+
+
+
+cd $SOURCE_DIR
+
+
+
+NAME=binary-app-installer
+VERSION=1.0
+
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+sudo rm master.tar.bz2
+wget -nc https://bitbucket.org/chandrakantsingh/binary-app-installer/get/master.tar.bz2 && sudo tar xf master.tar.bz2 -C /
+sudo update-desktop-database
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
+
