8735866ad3910c3b47e898e1a39f212d259dec1f
Chandrakant <aryalinux11@gmail.com>
2019-10-21 21:21:42 +0530

    Removed doc directory from getting backed up

--- base-system/backup.sh
+++ base-system/backup.sh
@@ -24,7 +24,16 @@ pushd /
 
 if [ ! -f $LFS/sources/aryalinux-$OS_VERSION-$LABEL-$(uname -m).tar.gz ]
 then
-	GZIP=-9 tar --exclude="$LFS/sources" --exclude="$LFS/usr/share/doc/*" --exclude="$LFS/tools" --exclude="$LFS/root/.ccache" --exclude="$LFS/home/aryalinux/.ccache" --exclude="$LFS/var/cache/alps/binaries" --exclude="$LFS/var/cache/alps/sources" -czvf $LFS/sources/aryalinux-$OS_VERSION-$LABEL-$(uname -m).tar.gz $LFS
+
+GZIP=-9 tar --exclude="$LFS/sources" \
+	--exclude="$LFS/usr/share/doc/*" \
+	--exclude="$LFS/tools" \
+	--exclude="$LFS/root/.ccache" \
+	--exclude="$LFS/root/.cargo" \
+	--exclude="$LFS/home/$USERNAME/.ccache" \
+	--exclude="$LFS/var/cache/alps/binaries" \
+	--exclude="$LFS/var/cache/alps/sources" \
+	-czvf $LFS/sources/aryalinux-$OS_VERSION-$LABEL-$(uname -m).tar.gz $LFS
 	if [ "$TOOLCHAIN_BACKUP" == "y" ] || [ "$TOOLCHAIN_BACKUP" == "Y" ] && [ ! -f $LFS/sources/toolchain-$OS_VERSION-$(uname -m).tar.xz ] ; then
 		XZ_OPT=-9 tar -cJvf $LFS/sources/toolchain-$OS_VERSION-$(uname -m).tar.xz $LFS/tools
 	fi
