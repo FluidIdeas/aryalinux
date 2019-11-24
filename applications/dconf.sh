22d5c1dcbff9d3cddc947a594fa677d4282dbe79
Chandrakant <aryalinux11@gmail.com>
2019-11-24 07:14:45 +0530

    Removed redundant scripts and fixed dconf-editor

--- applications/dconf.sh
+++ applications/dconf.sh
@@ -62,15 +62,6 @@ chmod a+x /tmp/rootscript.sh
 sudo /tmp/rootscript.sh
 sudo rm -rf /tmp/rootscript.sh
 
-cd ..              &&
-tar -xf ../dconf-editor-3.34.2.tar.xz &&
-cd dconf-editor-3.34.2                &&
-
-mkdir build &&
-cd    build &&
-
-meson --prefix=/usr --sysconfdir=/etc .. &&
-ninja
 sudo rm -rf /tmp/rootscript.sh
 cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
 ninja install
