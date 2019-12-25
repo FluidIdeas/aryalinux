eba47e9832abcc9da991bb9eb75ad77c3513d79e
Chandrakant Singh <aryalinux11@gmail.com>
2019-12-25 12:48:02 +0530

    Fixed rust

--- applications/rust.sh
+++ applications/rust.sh
@@ -10,7 +10,6 @@ set +h
 #REQ:curl
 #REQ:cmake
 #REQ:libssh2
-#REQ:llvm
 
 
 cd $SOURCE_DIR
@@ -19,7 +18,7 @@ wget -nc https://static.rust-lang.org/dist/rustc-1.37.0-src.tar.gz
 
 
 NAME=rust
-VERSION=1.37.
+VERSION=1.37.0
 URL=https://static.rust-lang.org/dist/rustc-1.37.0-src.tar.gz
 
 if [ ! -z $URL ]
@@ -38,35 +37,15 @@ fi
 cd $DIRECTORY
 fi
 
-echo $USER > /tmp/currentuser
+sudo mkdir /opt/rustc-1.37.0             &&
+sudo ln -svfin rustc-1.37.0 /opt/rustc
 
-
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-mkdir /opt/rustc-1.37.0             &&
-ln -svfin rustc-1.37.0 /opt/rustc
-ENDOFROOTSCRIPT
-
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-cat << EOF > config.toml
-# see config.toml.example for more possible options
-# See the 8.4 book for an example using shipped LLVM
-# e.g. if not installing clang, or using a version before 8.0.
+cat > config.toml << EOF
 [llvm]
-# by default, rust will build for a myriad of architectures
-targets = "X86_64"
-
-# When using system llvm prefer shared libraries
 link-shared = true
 
 [build]
-# omit docs to save time and space (default is to build them)
 docs = false
-
-# install cargo as well as rust
 extended = true
 
 [install]
@@ -76,41 +55,20 @@ docdir = "share/doc/rustc-1.37.0"
 [rust]
 channel = "stable"
 rpath = false
-
-# BLFS does not install the FileCheck executable from llvm,
-# so disable codegen tests
 codegen-tests = false
-
-[target.x86_64-unknown-linux-gnu]
-# NB the output of llvm-config (i.e. help options) may be
-# dumped to the screen when config.toml is parsed.
-llvm-config = "/usr/bin/llvm-config"
-
-[target.i686-unknown-linux-gnu]
-# NB the output of llvm-config (i.e. help options) may be
-# dumped to the screen when config.toml is parsed.
-llvm-config = "/usr/bin/llvm-config"
-
-
 EOF
+
 export RUSTFLAGS="$RUSTFLAGS -C link-args=-lffi" &&
 python3 ./x.py build --exclude src/tools/miri
+
 export LIBSSH2_SYS_USE_PKG_CONFIG=1 &&
 DESTDIR=${PWD}/install python3 ./x.py install &&
 unset LIBSSH2_SYS_USE_PKG_CONFIG
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-chown -R root:root install &&
-cp -a install/* /
-ENDOFROOTSCRIPT
 
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
+sudo chown -R root:root install &&
+sudo cp -a install/* /
 
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-cat >> /etc/ld.so.conf << EOF
+sudo tee -a /etc/ld.so.conf << EOF
 # Begin rustc addition
 
 /opt/rustc/lib
@@ -118,41 +76,20 @@ cat >> /etc/ld.so.conf << EOF
 # End rustc addition
 EOF
 
-ldconfig
-ENDOFROOTSCRIPT
+sudo ldconfig
 
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-cat > /etc/profile.d/rustc.sh << "EOF"
+sudo tee /etc/profile.d/rustc.sh << "EOF"
 # Begin /etc/profile.d/rustc.sh
 
 pathprepend /opt/rustc/bin           PATH
 
 # End /etc/profile.d/rustc.sh
 EOF
-ENDOFROOTSCRIPT
 
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-source /etc/profile.d/rustc.sh
-
-
-cat > /tmp/clearcargo.sh <<EOF
 if [ -d $HOME/.cargo ]; then
-	rm -rf $HOME/.cargo
+       rm -rf $HOME/.cargo
 fi
-EOF
-chmod a+x /tmp/clearcargo.sh
-/tmp/clearcargo.sh
-sed -i "s@$HOME@/root@g" /tmp/clearcargo.sh
-sudo /tmp/clearcargo.sh
-sudo rm -f /tmp/clearcargo.sh
+
 
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
