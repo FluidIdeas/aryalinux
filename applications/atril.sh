c878a667e963aa83519269156b1a68765ccdb9e5
Chandrakant Singh <aryalinux11@gmail.com>
2019-12-27 15:04:32 +0530

    Fixed atril

--- applications/atril.sh
+++ applications/atril.sh
@@ -11,12 +11,12 @@ set +h
 
 cd $SOURCE_DIR
 
-wget -nc https://pub.mate-desktop.org/releases/1.23/atril-1.23.1.tar.xz
+wget -nc https://github.com/mate-desktop/atril/releases/download/v1.22.3/atril-1.22.3.tar.xz
 
 
 NAME=atril
-VERSION=1.23.1
-URL=https://pub.mate-desktop.org/releases/1.23/atril-1.23.1.tar.xz
+VERSION=1.22.3
+URL=https://github.com/mate-desktop/atril/releases/download/v1.22.3/atril-1.22.3.tar.xz
 
 if [ ! -z $URL ]
 then
@@ -36,7 +36,6 @@ fi
 
 ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-static &&
 make
-
 sudo make install
 
 
