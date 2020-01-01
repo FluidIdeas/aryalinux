aa6ebe40254deb29407fd49c12fa050262ed4a6f
Chandrakant Singh <aryalinux11@gmail.com>
2020-01-02 00:15:00 +0530

    Fixed epdfview

--- applications/epdfview.sh
+++ applications/epdfview.sh
@@ -9,17 +9,17 @@ set +h
 
 #REQ:gtk2
 #REQ:poppler
-#REQ:desktop-file-utils
-#REQ:hicolor-icon-theme
 
 
 cd $SOURCE_DIR
 
-##DOWNLOADS##
+wget -nc http://anduin.linuxfromscratch.org/BLFS/epdfview/epdfview-0.1.8.tar.bz2
+wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/2.1/epdfview-0.1.8-fixes-2.patch
+
 
 NAME=epdfview
 VERSION=0.1.8
-##URL##
+URL=http://anduin.linuxfromscratch.org/BLFS/epdfview/epdfview-0.1.8.tar.bz2
 
 if [ ! -z $URL ]
 then
@@ -37,18 +37,37 @@ fi
 cd $DIRECTORY
 fi
 
+echo $USER > /tmp/currentuser
+
+
 patch -Np1 -i ../epdfview-0.1.8-fixes-2.patch &&
 ./configure --prefix=/usr &&
 make
-sudo make install
+sudo rm -rf /tmp/rootscript.sh
+cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
+make install
+ENDOFROOTSCRIPT
+
+chmod a+x /tmp/rootscript.sh
+sudo /tmp/rootscript.sh
+sudo rm -rf /tmp/rootscript.sh
+
+sudo rm -rf /tmp/rootscript.sh
+cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
 for size in 24 32 48; do
-  sudo ln -svf ../../../../epdfview/pixmaps/icon_epdfview-$size.png \
+  ln -svf ../../../../epdfview/pixmaps/icon_epdfview-$size.png \
           /usr/share/icons/hicolor/${size}x${size}/apps
 done &&
 unset size &&
 
-sudo update-desktop-database &&
-sudo gtk-update-icon-cache -t -f --include-image-data /usr/share/icons/hicolor
+update-desktop-database &&
+gtk-update-icon-cache -t -f --include-image-data /usr/share/icons/hicolor
+ENDOFROOTSCRIPT
+
+chmod a+x /tmp/rootscript.sh
+sudo /tmp/rootscript.sh
+sudo rm -rf /tmp/rootscript.sh
+
 
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
