0faedaace98b1966c02df622250af9dbf97bcdab
Chandrakant Singh <aryalinux11@gmail.com>
2020-01-09 11:31:43 +0530

    Added filezilla

---
+++ applications/filezilla.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+. /etc/alps/directories.conf
+
+
+
+cd $SOURCE_DIR
+
+wget -nc https://download.filezilla-project.org/client/FileZilla_3.46.3_src.tar.bz2
+
+
+NAME=filezilla
+VERSION=3.46.3
+
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+./configure --prefix=/usr && make -j$(nproc)
+sudo make install
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
+
