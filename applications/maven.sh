e2b940e045c8b24c2a9c520b418442f0f61eacba
Chandrakant <aryalinux11@gmail.com>
2020-01-15 17:14:44 +0530

    Added scripts for maven

---
+++ applications/maven.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+. /etc/alps/directories.conf
+
+#REQ:openjdk
+
+
+cd $SOURCE_DIR
+
+wget -nc https://www-us.apache.org/dist/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+
+
+NAME=maven
+VERSION=3.6.3
+
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+sudo tar xf apache-maven-3.6.3-bin.tar.gz -C /opt &&
+
+sudo tee /etc/profile.d/maven.sh <<"EOF"
+export PATH=$PATH:/opt/apache-maven-3.6.3/bin
+export M2_HOME=/opt/apache-maven-3.6.3
+EOF
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
+
