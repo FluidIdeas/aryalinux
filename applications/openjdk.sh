648f6a807675fdcbc35004b7140cbca71eacc894
Chandrakant <aryalinux11@gmail.com>
2020-01-15 16:48:40 +0530

    Updated openjdk script to install the binary available online

--- applications/openjdk.sh
+++ applications/openjdk.sh
@@ -7,33 +7,17 @@ set +h
 . /var/lib/alps/functions
 . /etc/alps/directories.conf
 
-#REQ:java
-#REQ:ojdk-conf
-#REQ:alsa-lib
-#REQ:cpio
 #REQ:cups
-#REQ:unzip
-#REQ:which
-#REQ:x7lib
-#REQ:zip
-#REQ:make-ca
-#REQ:giflib
-#REQ:lcms2
-#REQ:libjpeg
-#REQ:libpng
-#REQ:wget
 
 
 cd $SOURCE_DIR
 
-wget -nc http://hg.openjdk.java.net/jdk-updates/jdk12u/archive/jdk-12.0.2+10.tar.bz2
-wget -nc http://anduin.linuxfromscratch.org/BLFS/OpenJDK/OpenJDK-12.0.2/jtreg-4.2-b13-517.tar.gz
+wget -nc https://download.java.net/java/GA/jdk13.0.2/d4173c853231432d94f001e99d882ca7/8/GPL/openjdk-13.0.2_linux-x64_bin.tar.gz
 
 
 NAME=openjdk
-VERSION=12.0.
-URL=http://hg.openjdk.java.net/jdk-updates/jdk12u/archive/jdk-12.0.2+10.tar.bz2
-SECTION="Miscellaneous"
+VERSION=13.0.2
+
 
 if [ ! -z $URL ]
 then
@@ -51,61 +35,17 @@ fi
 cd $DIRECTORY
 fi
 
-echo $USER > /tmp/currentuser
+sudo tar xf openjdk-13.0.2_linux-x64_bin.tar.gz -C /opt &&
 
+sudo tee /etc/profile.d/openjdk.sh <<"EOF"
+export PATH=$PATH:/opt/jdk-13.0.2/bin
+export JAVA_HOME=/opt/jdk-13.0.2
+EOF
 
-tar -xf ../jtreg-4.2-b13-517.tar.gz
-unset JAVA_HOME                             &&
-bash configure --enable-unlimited-crypto    \
-               --disable-warnings-as-errors \
-               --with-stdc++lib=dynamic     \
-               --with-giflib=system         \
-               --with-jtreg=$PWD/jtreg      \
-               --with-lcms=system           \
-               --with-libjpeg=system        \
-               --with-libpng=system         \
-               --with-zlib=system           \
-               --with-version-build="10"    \
-               --with-version-pre=""        \
-               --with-version-opt=""        \
-               --with-cacerts-file=/etc/pki/tls/java/cacerts &&
-make images
-export JT_JAVA=$(echo $PWD/build/*/jdk) &&
-jtreg/bin/jtreg -jdk:$JT_JAVA -automatic -ignore:quiet -v1 \
-    test/jdk:tier1 test/langtools:tier1 &&
-unset JT_JAVA
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-install -vdm755 /opt/jdk-12.0.2+10             &&
-cp -Rv build/*/images/jdk/* /opt/jdk-12.0.2+10 &&
-chown -R root:root /opt/jdk-12.0.2+10          &&
-for s in 16 24 32 48; do
-  install -vDm644 src/java.desktop/unix/classes/sun/awt/X11/java-icon${s}.png \
-                  /usr/share/icons/hicolor/${s}x${s}/apps/java.png
-done
-ENDOFROOTSCRIPT
-
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-ln -v -nsf jdk-12.0.2+10 /opt/jdk
-ENDOFROOTSCRIPT
-
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-mkdir -pv /usr/share/applications &&
-
-cat > /usr/share/applications/openjdk-java.desktop << "EOF" &&
+sudo tee /usr/share/applications/openjdk-java.desktop << "EOF" &&
 [Desktop Entry]
-Name=OpenJDK Java 12.0.2 Runtime
-Comment=OpenJDK Java 12.0.2 Runtime
+Name=OpenJDK Java 13.0.2 Runtime
+Comment=OpenJDK Java 13.0.2 Runtime
 Exec=/opt/jdk/bin/java -jar
 Terminal=false
 Type=Application
@@ -113,10 +53,11 @@ Icon=java
 MimeType=application/x-java-archive;application/java-archive;application/x-jar;
 NoDisplay=true
 EOF
-cat > /usr/share/applications/openjdk-jconsole.desktop << "EOF"
+
+sudo tee /usr/share/applications/openjdk-jconsole.desktop << "EOF"
 [Desktop Entry]
-Name=OpenJDK Java 12.0.2 Console
-Comment=OpenJDK Java 12.0.2 Console
+Name=OpenJDK Java 13.0.2 Console
+Comment=OpenJDK Java 13.0.2 Console
 Keywords=java;console;monitoring
 Exec=/opt/jdk/bin/jconsole
 Terminal=false
@@ -124,31 +65,8 @@ Type=Application
 Icon=java
 Categories=Application;System;
 EOF
-ENDOFROOTSCRIPT
 
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-ln -sfv /etc/pki/tls/java/cacerts /opt/jdk/lib/security/cacerts
-ENDOFROOTSCRIPT
-
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-cd /opt/jdk
-bin/keytool -list -cacerts
-ENDOFROOTSCRIPT
-
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
+sudo update-desktop-database
 
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
