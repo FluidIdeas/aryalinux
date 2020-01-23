b2b434d72d689b56784f63dca441fa42793bd15a
Chandrakant <aryalinux11@gmail.com>
2020-01-23 18:42:30 +0530

    Made proper the mongodb installation part involving systemd service and config file

--- applications/mongodb.sh
+++ applications/mongodb.sh
@@ -40,9 +40,52 @@ sudo pip3 install psutil
 sudo pip install psutil
 sudo pip3 install Cheetah3
 sudo pip install Cheetah
+groupadd -g 49 mongodb &&
+useradd -c "MongoDB User" -g mongodb -d /var/lib/mongodb \
+        -u 49 mongodb
+sudo mkdir -pv /var/lib/mongodb/
+sudo mkdir -pv /var/run/mongodb/
+sudo chown mongodb:mongodb /var/lib/mongodb/
+sudo chown mongodb:mongodb /var/run/mongodb/
+
 scons core --disable-warnings-as-errors -j$(nproc) install &&
 sudo cp -v build/opt/mongo/mongo* /usr/bin
 
+sudo tee /lib/systemd/system/mongodb.service<<EOF
+[Unit]
+Description=MongoDB Database Server
+Documentation=https://docs.mongodb.org/manual
+After=network.target
+
+[Service]
+User=mongodb
+Group=mongodb
+ExecStart=/usr/bin/mongod --config /etc/mongod.conf
+PIDFile=/var/run/mongodb/mongod.pid
+# file size
+LimitFSIZE=infinity
+# cpu time
+LimitCPU=infinity
+# virtual memory size
+LimitAS=infinity
+# open files
+LimitNOFILE=64000
+# processes/threads
+LimitNPROC=64000
+# locked memory
+LimitMEMLOCK=infinity
+# total threads (user+kernel)
+TasksMax=infinity
+TasksAccounting=false
+
+# Recommended limits for for mongod as specified in
+# http://docs.mongodb.org/manual/reference/ulimit/#recommended-settings
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo cp debian/mongod.conf /etc/
+sudo systemctl enable mongodb
 
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
