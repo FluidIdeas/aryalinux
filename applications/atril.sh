1afda114a6ffd3b9c1b29d1eaa3016f879e7278f
Chandrakant <aryalinux11@gmail.com>
2020-01-25 21:21:37 +0530

    Updated atril and changed lsb-release description

--- applications/atril.sh
+++ applications/atril.sh
@@ -11,13 +11,12 @@ set +h
 
 cd $SOURCE_DIR
 
-wget -nc https://pub.mate-desktop.org/releases/1.23/atril-1.23.2.tar.xz
+wget -nc https://github.com/mate-desktop/atril/releases/download/v1.22.3/atril-1.22.3.tar.xz
 
 
 NAME=atril
-VERSION=1.23.2
-URL=https://pub.mate-desktop.org/releases/1.23/atril-1.23.2.tar.xz
-SECTION="Mate Desktop Applications"
+VERSION=1.22.3
+URL=https://github.com/mate-desktop/atril/releases/download/v1.22.3/atril-1.22.3.tar.xz
 DESCRIPTION="Atril is a document viewer capable of displaying multiple and single page document formats like PDF and Postscript."
 
 if [ ! -z $URL ]
@@ -38,7 +37,6 @@ fi
 
 ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-static &&
 make
-
 sudo make install
 
 
