3c6f6af94f144be62f9500ccc2eeefc7293f5eb6
Chandrakant Singh <aryalinux11@gmail.com>
2020-02-18 06:21:29 +0530

    Updated kernel version in several places

--- base-system/additional-downloads.sh
+++ base-system/additional-downloads.sh
@@ -3,7 +3,7 @@
 set -e
 set +h
 
-VERSION=2.1
+VERSION=2.4
 
 CURRENT_DIR=$(pwd)
 pushd ~/sources
@@ -44,20 +44,14 @@ wget -nc http://gondor.apana.org.au/~herbert/dash/files/dash-0.5.9.1.tar.gz
 
 wget -c https://github.com/dracutdevs/dracut/archive/049.tar.gz -O dracut-049.tar.gz
 
-#wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/399ee7eb552ec30015e1a7e07e357f5815d25951/grub-2.02-gcc.patch
 wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/b09bd478061f93f986442bf654bb3fe8fadb59f7/efibootmgr-17-efidir.patch
 wget -nc https://sourceware.org/ftp/lvm2/releases/LVM2.2.02.171.tgz
-wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/aufs-5.2.tar.gz
+wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/aufs-5.5.tar.gz
 wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/aufs5-base.patch
 wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/aufs5-kbuild.patch
 wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/aufs5-mmap.patch
 wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/aufs5-standalone.patch
 wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/aufs5-loopback.patch
-#wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/aufs5-lockdep-debug.patch
-#wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/aufs5-proc_mounts.patch
-#wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/aufs5-tmpfs-idr.patch
-#wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/aufs5-vfs-ino.patch
-#wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/grub-2.02-relocation.patch
 wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/$VERSION/squashfs-tools-4.3-sysmacros.patch
 wget -nc https://sourceware.org/ftp/elfutils/0.170/elfutils-0.170.tar.bz2
 wget -nc https://bitbucket.org/chandrakantsingh/patches/raw/1.1/0.21-nvme_ioctl.h.patch
