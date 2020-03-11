acb4ced2bcb131f6c6eb0d4c79ef6967a15531e8
Chandrakant <aryalinux11@gmail.com>
2020-03-11 09:09:00 +0530

    Fixed the description of libostree

--- applications/libostree.sh
+++ applications/libostree.sh
@@ -18,7 +18,7 @@ wget -nc https://github.com/ostreedev/ostree/releases/download/v2019.4/libostree
 NAME=libostree
 VERSION=2019.4
 URL=https://github.com/ostreedev/ostree/releases/download/v2019.4/libostree-2019.4.tar.xz
-DESCRIPTION="libostree is a library for managing bootable, immutable, versioned filesystem trees. It is like git in that it checksums individual files and has a content-addressed object store; unlike git, it "checks out" the files using hardlinks into an immutable directory tree. This can be used to provide atomic upgrades with rollback, history and parallel-installation, particularly useful on fixed purpose systems such as embedded devices. It is also used by the Flatpak application runtime system."
+DESCRIPTION="libostree is a library for managing bootable, immutable, versioned filesystem trees. It is like git in that it checksums individual files and has a content-addressed object store; unlike git, it checks out the files using hardlinks into an immutable directory tree. This can be used to provide atomic upgrades with rollback, history and parallel-installation, particularly useful on fixed purpose systems such as embedded devices. It is also used by the Flatpak application runtime system."
 
 if [ ! -z $URL ]
 then
