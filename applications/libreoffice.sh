6851e8f45caf60040cb14d143c1efad6bb507182
Chandrakant <aryalinux11@gmail.com>
2020-03-12 06:41:10 +0530

    Updated libreoffice and rust to install from binaries

--- applications/libreoffice.sh
+++ applications/libreoffice.sh
@@ -45,15 +45,12 @@ set +h
 
 cd $SOURCE_DIR
 
-wget -nc http://download.documentfoundation.org/libreoffice/src/6.4.0/libreoffice-6.4.0.3.tar.xz
-wget -nc http://download.documentfoundation.org/libreoffice/src/6.4.0/libreoffice-dictionaries-6.4.0.3.tar.xz
-wget -nc http://download.documentfoundation.org/libreoffice/src/6.4.0/libreoffice-help-6.4.0.3.tar.xz
-wget -nc http://download.documentfoundation.org/libreoffice/src/6.4.0/libreoffice-translations-6.4.0.3.tar.xz
+wget -nc http://aryalinux.info/files/2.4/libreoffice-6.4.0.3-x86_64.tar.gz
 
 
 NAME=libreoffice
 VERSION=6.4.0.3
-URL=http://download.documentfoundation.org/libreoffice/src/6.4.0/libreoffice-6.4.0.3.tar.xz
+URL=http://aryalinux.info/files/2.4/libreoffice-6.4.0.3-x86_64.tar.gz
 SECTION="Office Programs"
 DESCRIPTION="LibreOffice is a full-featured office suite. It is largely compatible with Microsoft Office and is descended from OpenOffice.org."
 
@@ -73,89 +70,7 @@ fi
 cd $DIRECTORY
 fi
 
-echo $USER > /tmp/currentuser
-
-
-install -dm755 external/tarballs &&
-ln -sv ../../../libreoffice-dictionaries-6.4.0.3.tar.xz external/tarballs/ &&
-ln -sv ../../../libreoffice-help-6.4.0.3.tar.xz         external/tarballs/ &&
-ln -sv ../../../libreoffice-translations-6.4.0.3.tar.xz external/tarballs/
-export LO_PREFIX=/usr
-sed -e "/gzip -f/d"   \
-    -e "s|.1.gz|.1|g" \
-    -i bin/distro-install-desktop-integration &&
-
-sed -e "/distro-install-file-lists/d" -i Makefile.in &&
-
-sed -e '/JAVA_SOURCE_VER/s/6/7/' \
-    -e '/JAVA_TARGET_VER/s/6/7/' \
-    -i configure.ac &&
-
-sed -e 's/globalParams = new GlobalParams()/globalParams.reset(new GlobalParams())/' \
-    -i sdext/source/pdfimport/xpdfwrapper/wrapper_gpl.cxx &&
-
-sed -e 's/printPath( GfxPath/printPath(const GfxPath/' \
-    -e 's/  GfxSubpath/ const GfxSubpath/' \
-    -i sdext/source/pdfimport/xpdfwrapper/pdfioutdev_gpl.* &&
-
-./autogen.sh --prefix=$LO_PREFIX         \
-             --sysconfdir=/etc           \
-             --with-vendor=AryaLinux          \
-             --with-lang=ALL --without-java      \
-             --with-help                 \
-             --with-myspell-dicts        \
-             --without-junit             \
-             --without-system-dicts      \
-             --disable-dconf             \
-             --disable-odk               \
-             --enable-release-build=yes  \
-             --enable-python=system      \
-             --with-jdk-home=/opt/jdk    \
-             --with-system-apr           \
-             --with-system-boost         \
-             --with-system-clucene       \
-             --with-system-curl          \
-             --with-system-epoxy         \
-             --with-system-expat         \
-             --with-system-glm           \
-             --with-system-gpgmepp       \
-             --with-system-graphite      \
-             --with-system-harfbuzz      \
-             --with-system-icu           \
-             --with-system-jpeg          \
-             --with-system-lcms2         \
-             --with-system-libatomic_ops \
-             --with-system-libpng        \
-             --with-system-libxml        \
-             --with-system-neon          \
-             --with-system-nss           \
-             --with-system-odbc          \
-             --with-system-openldap      \
-             --with-system-openssl       \
-             --with-system-poppler       \
-             --disable-postgresql-sdbc    \
-             --with-system-redland       \
-             --with-system-serf          \
-             --with-system-zlib
-make build-nocheck
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-make distro-pack-install
-ENDOFROOTSCRIPT
-
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
-sudo rm -rf /tmp/rootscript.sh
-cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-update-desktop-database
-ENDOFROOTSCRIPT
-
-chmod a+x /tmp/rootscript.sh
-sudo /tmp/rootscript.sh
-sudo rm -rf /tmp/rootscript.sh
-
+sudo tar xf libreoffice-6.4.0.3-x86_64.tar.gz -C /
 
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
