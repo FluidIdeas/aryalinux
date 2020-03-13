c0a9c2d3d04edf6cd76484f5857d17096fd17a79
Chandrakant <aryalinux11@gmail.com>
2020-03-14 02:01:17 +0530

    Added graphene, a mutter dependency

---
+++ applications/graphene.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+set +h
+
+. /etc/alps/alps.conf
+. /var/lib/alps/functions
+. /etc/alps/directories.conf
+
+
+
+cd $SOURCE_DIR
+
+wget -nc ftp://ftp.gnome.org/pub/gnome/sources/graphene/1.9/graphene-1.9.6.tar.xz
+
+
+NAME=graphene
+VERSION=1.9.6
+URL=ftp://ftp.gnome.org/pub/gnome/sources/graphene/1.9/graphene-1.9.6.tar.xz
+SECTION="Others"
+DESCRIPTION=""
+
+if [ ! -z $URL ]
+then
+
+TARBALL=$(echo $URL | rev | cut -d/ -f1 | rev)
+if [ -z $(echo $TARBALL | grep ".zip$") ]; then
+	DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq | grep -v "^\.$")
+	sudo rm -rf $DIRECTORY
+	tar --no-overwrite-dir -xf $TARBALL
+else
+	DIRECTORY=$(unzip_dirname $TARBALL $NAME)
+	unzip_file $TARBALL $NAME
+fi
+
+cd $DIRECTORY
+fi
+
+mkdir build
+cd build
+
+meson --prefix=/usr &&
+ninja
+
+sudo ninja install
+
+
+if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
+
+register_installed "$NAME" "$VERSION" "$INSTALLED_LIST"
+
