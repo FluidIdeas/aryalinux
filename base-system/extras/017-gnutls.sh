1caf3fd9aa2583f9687fc266b15d7341c10a1a3c
Chandrakant Singh <chandrakant.singh@syngenta.com>
2021-04-18 13:47:59 +0530

    Fixed gnutls

--- base-system/extras/017-gnutls.sh
+++ base-system/extras/017-gnutls.sh
@@ -32,6 +32,7 @@ fi
 ./configure --prefix=/usr \
             --docdir=/usr/share/doc/gnutls-3.7.0 \
             --disable-guile \
+            --with-included-unistring \
             --with-default-trust-store-pkcs11="pkcs11:" &&
 make
 make install
