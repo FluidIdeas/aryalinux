3592d8d382424e52e45e817e0449c00314ac342a
Chandrakant Singh <chandrakant.singh@syngenta.com>
2021-04-21 07:47:03 +0530

    Fixed gtk3

--- applications/gtk3.sh
+++ applications/gtk3.sh
@@ -67,13 +67,21 @@ chmod a+x /tmp/rootscript.sh
 sudo /tmp/rootscript.sh
 sudo rm -rf /tmp/rootscript.sh
 
+mkdir -pv ~/.config/gtk-3.0
+mkdir -pv /etc/skel/.config/gtk-3.0
+
+cat > /etc/skel/.config/gtk-3.0/gtk.css << "EOF"
+*  {
+   -GtkScrollbar-has-backward-stepper: 1;
+   -GtkScrollbar-has-forward-stepper: 1;
+}
+EOF
 cat > ~/.config/gtk-3.0/gtk.css << "EOF"
 *  {
    -GtkScrollbar-has-backward-stepper: 1;
    -GtkScrollbar-has-forward-stepper: 1;
 }
 EOF
-
 
 if [ ! -z $URL ]; then cd $SOURCE_DIR && cleanup "$NAME" "$DIRECTORY"; fi
 
