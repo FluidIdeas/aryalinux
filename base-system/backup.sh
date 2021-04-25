58140a101c92bad2d391a3dacee6d64886ecbc76
Chandrakant Singh <chandrakant.singh@syngenta.com>
2021-04-25 14:03:15 +0530

    Removed Linux source directory from backup

--- base-system/backup.sh
+++ base-system/backup.sh
@@ -33,6 +33,7 @@ GZIP=-9 tar --exclude="$LFS/sources" \
 	--exclude="$LFS/home/$USERNAME/.ccache" \
 	--exclude="$LFS/var/cache/alps/binaries" \
 	--exclude="$LFS/var/cache/alps/sources" \
+	--exclude="$LFS/usr/src/*" \
 	-czvf $LFS/sources/aryalinux-$OS_VERSION-$LABEL-$(uname -m).tar.gz $LFS
 	if [ "$TOOLCHAIN_BACKUP" == "y" ] || [ "$TOOLCHAIN_BACKUP" == "Y" ] && [ ! -f $LFS/sources/toolchain-$OS_VERSION-$(uname -m).tar.xz ] ; then
 		XZ_OPT=-9 tar -cJvf $LFS/sources/toolchain-$OS_VERSION-$(uname -m).tar.xz $LFS/tools
