210713a7081995aad3df048fe405b51831b3d71b
Chandrakant Singh <chandrakant.singh@syngenta.com>
2021-05-14 15:35:37 +0530

    Bumped xfce4-screenshooter

--- applications/xfce4-screenshooter.sh
+++ applications/xfce4-screenshooter.sh
@@ -11,12 +11,12 @@ set +h
 
 cd $SOURCE_DIR
 
-wget -nc https://github.com/xfce-mirror/xfce4-screenshooter/archive/xfce4-screenshooter-1.9.7.tar.gz
+wget -nc https://github.com/xfce-mirror/xfce4-screenshooter/archive/xfce4-screenshooter-1.9.8.tar.gz
 
 
 NAME=xfce4-screenshooter
-VERSION=1.9.7
-URL=https://github.com/xfce-mirror/xfce4-screenshooter/archive/xfce4-screenshooter-1.9.7.tar.gz
+VERSION=1.9.8
+URL=https://github.com/xfce-mirror/xfce4-screenshooter/archive/xfce4-screenshooter-1.9.8.tar.gz
 DESCRIPTION="Screenshooter is an utility for the Xfce Desktop Environment. It can take desktop, rectangles or selected window screenshots, and you can bind it to your 'Print Screen' key. A panel plugin is provided too."
 
 if [ ! -z $URL ]
