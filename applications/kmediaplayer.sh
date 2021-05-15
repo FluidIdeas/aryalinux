9374569e7283d639a724e706a850dce21ff19663
Chandrakant Singh <chandrakant.singh@syngenta.com>
2021-05-15 16:14:43 +0530

    Added kmediaplayer

--- applications/kmediaplayer.sh
+++ applications/kmediaplayer.sh
@@ -11,11 +11,11 @@ set +h
 
 cd $SOURCE_DIR
 
-wget -nc https://download.kde.org/stable/frameworks/5.82/kmediaplayer-5.82.0.tar.xz
+wget -nc https://github.com/KDE/kmediaplayer/archive/v5.82.0/kmediaplayer-5.82.0.tar.gz
 
 NAME=kmediaplayer
 VERSION=5.82.0
-URL=https://download.kde.org/stable/frameworks/5.82/kmediaplayer-5.82.0.tar.xz
+URL=https://github.com/KDE/kmediaplayer/archive/v5.82.0/kmediaplayer-5.82.0.tar.gz
 SECTION="KDE Plasma 5"
 
 if [ ! -z $URL ]
