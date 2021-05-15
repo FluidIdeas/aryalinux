b9355eacb0e21081da55bd57bf2a2f2b441ad044
Chandrakant Singh <chandrakant.singh@syngenta.com>
2021-05-15 17:40:08 +0530

    Bumped plasma-firewall

--- applications/plasma-firewall.sh
+++ applications/plasma-firewall.sh
@@ -11,11 +11,11 @@ set +h
 
 cd $SOURCE_DIR
 
-wget -nc https://github.com/KDE/plasma-firewall/archive/v5.18.7/plasma-firewall-5.18.7.tar.gz
+wget -nc https://github.com/KDE/plasma-firewall/archive/v5.21.90/plasma-firewall-5.21.90.tar.gz
 
 NAME=plasma-firewall
-VERSION=5.18.7
-URL=https://github.com/KDE/plasma-firewall/archive/v5.18.7/plasma-firewall-5.18.7.tar.gz
+VERSION=5.21.90
+URL=https://github.com/KDE/plasma-firewall/archive/v5.21.90/plasma-firewall-5.21.90.tar.gz
 SECTION="KDE Plasma 5"
 
 if [ ! -z $URL ]
