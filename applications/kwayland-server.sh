23259831e6ebfdb6364fd2efe69dda260526e72a
Chandrakant Singh <chandrakant.singh@syngenta.com>
2021-05-15 17:08:12 +0530

    Bumped kwayland-server

--- applications/kwayland-server.sh
+++ applications/kwayland-server.sh
@@ -11,11 +11,11 @@ set +h
 
 cd $SOURCE_DIR
 
-wget -nc https://github.com/KDE/kwayland-server/archive/v5.18.7/kwayland-server-5.18.7.tar.gz
+wget -nc https://github.com/KDE/kwayland-server/archive/v5.21.90/kwayland-server-5.21.90.tar.gz
 
 NAME=kwayland-server
-VERSION=5.18.7
-URL=https://github.com/KDE/kwayland-server/archive/v5.18.7/kwayland-server-5.18.7.tar.gz
+VERSION=5.21.90
+URL=https://github.com/KDE/kwayland-server/archive/v5.21.90/kwayland-server-5.21.90.tar.gz
 SECTION="KDE Plasma 5"
 
 if [ ! -z $URL ]
