83aad2b95d0d8622a28f8dd0fd2578cbef750832
Chandrakant Singh <chandrakant.singh@syngenta.com>
2021-12-22 15:28:24 +0530

    fixed stage6.sh

--- base-system/stage6.sh
+++ base-system/stage6.sh
@@ -35,4 +35,4 @@ mount -vt tmpfs tmpfs $LFS/dev/shm
 chroot "$LFS" /usr/bin/env -i              \
     HOME=/root TERM="$TERM" PS1='\u:\w\$ ' \
     PATH=/bin:/usr/bin:/sbin:/usr/sbin     \
-    bash --login $LFS/sources/stage7.sh
+    bash --login /sources/stage7.sh
