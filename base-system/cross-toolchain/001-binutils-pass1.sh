1fedbd7298cfc17927c6ed89af14d1343e3e22dd
Chandrakant Singh <chandrakant.singh@syngenta.com>
2022-02-18 21:54:12 +0530

    Fixed binutils scripts

--- base-system/cross-toolchain/001-binutils-pass1.sh
+++ base-system/cross-toolchain/001-binutils-pass1.sh
@@ -13,7 +13,7 @@ if ! grep "$NAME" /sources/build-log; then
 
 cd /sources
 
-TARBALL=binutils-2.38-lto_fix-1.patch
+TARBALL=binutils-2.38.tar.xz
 DIRECTORY=$(tar tf $TARBALL | cut -d/ -f1 | uniq)
 
 tar xf $TARBALL
