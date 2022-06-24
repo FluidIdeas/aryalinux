67437c5842c59efad3ba69a4598363cb9da11f37
Chandrakant Singh <chandrakantsingh@protonmail.com>
2022-06-24 15:01:52 +0530

    Fixed libgcrypt

--- applications/libgcrypt.sh
+++ applications/libgcrypt.sh
@@ -48,24 +48,9 @@ echo $USER > /tmp/currentuser
 ./configure --prefix=/usr &&
 make                      &&
 
-make -C doc html                                                       &&
-makeinfo --html --no-split -o doc/gcrypt_nochunks.html doc/gcrypt.texi &&
-makeinfo --plaintext       -o doc/gcrypt.txt           doc/gcrypt.texi
-make -C doc pdf
 sudo rm -rf /tmp/rootscript.sh
 cat > /tmp/rootscript.sh <<"ENDOFROOTSCRIPT"
-make install &&
-install -v -dm755   /usr/share/doc/libgcrypt-1.10.1 &&
-install -v -m644    README doc/{README.apichanges,fips*,libgcrypt*} \
-                    /usr/share/doc/libgcrypt-1.10.1 &&
-
-install -v -dm755   /usr/share/doc/libgcrypt-1.10.1/html &&
-install -v -m644 doc/gcrypt.html/* \
-                    /usr/share/doc/libgcrypt-1.10.1/html &&
-install -v -m644 doc/gcrypt_nochunks.html \
-                    /usr/share/doc/libgcrypt-1.10.1      &&
-install -v -m644 doc/gcrypt.{txt,texi} \
-                    /usr/share/doc/libgcrypt-1.10.1
+make install
 ENDOFROOTSCRIPT
 
 chmod a+x /tmp/rootscript.sh
