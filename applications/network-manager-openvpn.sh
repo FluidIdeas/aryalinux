1e875bfd6de939c9d9ec939ba9b7c6a826ab2407
Chandrakant Singh <chandrakantsingh@protonmail.com>
2022-06-29 11:55:42 +0530

    Upgraded nm-openvpn

--- applications/network-manager-openvpn.sh
+++ applications/network-manager-openvpn.sh
@@ -40,7 +40,7 @@ cd $DIRECTORY
 fi
 
 if grep "gnome-desktop-environment" /etc/alps/installed-list &> /dev/null; then WITH_GNOME="--with-gnome"; fi
-./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --without-libnm-glib $WITH_GNOME &&
+./autogen.sh --prefix=/usr --sysconfdir=/etc --localstatedir=/var --without-libnm-glib $WITH_GNOME &&
 make
 sudo make install
 
