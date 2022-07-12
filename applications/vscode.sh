4c9afdbbc77a5f7872cef72d3df25085c4f46173
Chandrakant Singh <chandrakantsingh@protonmail.com>
2022-07-12 06:40:36 +0530

    Fixed vscode

--- applications/vscode.sh
+++ applications/vscode.sh
@@ -39,9 +39,10 @@ cd $DIRECTORY
 fi
 
 version="16.16.0"
-wget https://az764295.vo.msecnd.net/stable/92d25e35d9bf1a6b16f7d0758f25d48ace11e5b9/code-stable-x64-$version.tar.gz
-dir=$(tar tf code-stable-x64-$version.tar.gz | cut -d/ -f1 | uniq)
-sudo tar xf code-stable-x64-$version.tar.gz -C /opt/
+wget --content-disposition https://code.visualstudio.com/sha/download?build=stable&os=linux-x64
+tarball=$(ls code-stable-x64*.tar.gz)
+dir=$(tar tf $tarball | cut -d/ -f1 | uniq)
+sudo tar xf $tarball -C /opt/
 
 sudo tee /usr/share/applications/code.desktop <<"EOF"
 [Desktop Entry]
