514b09f93bc58bad703c7f432a3b28c63e9ddfa0
Chandrakant Singh <cks3383@gmail.com>
2026-06-07 04:25:01 +0530

    Updated download list

--- base-system/initramfs.sh
+++ base-system/initramfs.sh
@@ -29,13 +29,13 @@ make install
 cd /sources
 rm -rf dash-0.5.13.1
 
-tar xf dracut-110.tar.xz
-cd dracut-110
+tar xf 110.tar.gz
+cd dracut-ng-110
 ./configure --disable-documentation
 make
 make install
 cd /sources
-rm -rf dracut-110
+rm -rf dracut-ng-110
 
 echo "initramfs" | tee -a /sources/build-log
 
